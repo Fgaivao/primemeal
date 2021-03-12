9d448ff89c166f95865a08e1951a9abf8aed8669
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-03-12 11:32:38 +0000

    protect app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   respond_to :json
 
